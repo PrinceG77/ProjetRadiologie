70a9ddd60354fbd1d1f57f82529b643681d6c5c3
PrinceG77 <princemalere7@gmail.com>
2023-02-28 20:16:24 +0100

    Radiologie

---
+++ src/test/java/com/example/projetradiologie/ProjetRadiologieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetradiologie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetRadiologieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
