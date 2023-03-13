7205c03a5a8bcf8d91d25f4e3b0c575e1b98267a
PrinceG77 <princemalere7@gmail.com>
2023-03-13 21:04:26 +0100

    Radiologie

--- src/main/java/com/example/projetradiologie/dao/SeanceDao.java
+++ src/main/java/com/example/projetradiologie/dao/SeanceDao.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface SeanceDao extends JpaRepository <Seance, Long> {
 
     Seance findByPrescriptionDatePrescription(LocalDateTime datePrescription);
+
+    List<Seance> findByPrescriptionPatientCin(String cin);
 
 }
