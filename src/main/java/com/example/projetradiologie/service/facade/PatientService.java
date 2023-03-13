e176b37face5a3c2729491e2ad912e2c37c848e8
PrinceG77 <princemalere7@gmail.com>
2023-03-13 21:24:37 +0100

    Radiologie

---
+++ src/main/java/com/example/projetradiologie/service/facade/PatientService.java
@@ -0,0 +1,20 @@
+package com.example.projetradiologie.service.facade;
+
+import com.example.projetradiologie.bean.Patient;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PatientService{
+    Patient findByCin(String cin);
+    @Transactional
+    int deleteByCin(String cin);
+
+    public List<Patient> findAll();
+
+    public int save(Patient patient);
+
+
+}
---
+++ src/main/java/com/example/projetradiologie/service/impl/PatientServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.projetradiologie.service.impl;
+
+import com.example.projetradiologie.bean.Patient;
+import com.example.projetradiologie.dao.PatientDao;
+import com.example.projetradiologie.service.facade.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+
+import java.util.List;
+
+public class PatientServiceImpl implements PatientService {
+
+    @Autowired
+    PatientDao patientDao;
+
+    @Override
+    public Patient findByCin(String cin) {
+        return patientDao.findByCin(cin);
+    }
+
+    @Override
+    public int deleteByCin(String cin) {
+        return patientDao.deleteByCin(cin);
+    }
+
+    public  List<Patient> findAll() {
+        return patientDao.findAll();
+    }
+
+    public int save(Patient patient) {
+        if(patientDao.findByCin(patient.getCin()) != null)
+        {
+            return -1;
+        }
+        else {
+            patientDao.save(patient);
+            return 1;
+        }
+
+    }
+}
