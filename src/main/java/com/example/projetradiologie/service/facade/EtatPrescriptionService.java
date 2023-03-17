a1d087c2babba7c3f71fa52fe0dba0c8733cd089
PrinceG77 <princemalere7@gmail.com>
2023-03-17 09:47:39 +0100

    Radiologie

--- src/main/java/com/example/projetradiologie/service/facade/EtatPrescriptionService.java
+++ src/main/java/com/example/projetradiologie/service/facade/EtatPrescriptionService.java
@@ -1,5 +1,6 @@
 package com.example.projetradiologie.service.facade;
 
+import com.example.projetradiologie.bean.EtatPrescription;
 import com.example.projetradiologie.bean.HistoriquePrescription;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,12 @@ import java.util.List;
 @Service
 public interface EtatPrescriptionService {
 
+    public EtatPrescription findByCode(Long Code);
 
+    public int deleteByCode(Long code);
+
+    public List<EtatPrescription> findAll();
+
+    public  int save(EtatPrescription etatPrescription);
 
 }
